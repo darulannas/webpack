6350e3d98c63dd1a59e295a56f05ba2efcf05cbf
darulannas <darulannas7@gmail.com>
2023-03-12 19:55:03 +0700

    asset management

--- belajar-webpack-code/webpack.dev.js
+++ belajar-webpack-code/webpack.dev.js
@@ -1,13 +1,24 @@
 const path = require("path");
 const config = require("./webpack.config");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "img/[hash][ext]",
     clean: true,
   },
+  plugins: [new MiniCssExtractPlugin()],
   watch: true,
 });
